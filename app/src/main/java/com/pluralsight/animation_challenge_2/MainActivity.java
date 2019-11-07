a77f018feb0cb8f230850542db806b13d688618b
alabiboo <aalabiboo@gmail.com>
2019-11-07 11:47:53 +0000

    first commit

---
+++ app/src/main/java/com/pluralsight/animation_challenge_2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.pluralsight.animation_challenge_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.List;
+
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
+import jp.wasabeef.recyclerview.animators.OvershootInRightAnimator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+        Landscape  landscapeList = new Landscape();
+
+        RecyclerAdapter adapter = new RecyclerAdapter(this, landscapeList.getData());
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new ScaleInAnimationAdapter(adapter));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setItemAnimator(new OvershootInRightAnimator());
+        recyclerView.getItemAnimator().setAddDuration(500);
+        recyclerView.getItemAnimator().setRemoveDuration(500);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/pluralsight/animation_challenge_2/RecyclerAdapter.java
@@ -0,0 +1,107 @@
+package com.pluralsight.animation_challenge_2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerAdapter extends  RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>{
+
+    private final Context mContext;
+    private  final List<Landscape> mLandscapes;
+    private final LayoutInflater mLayoutInflater;
+
+    public RecyclerAdapter(Context mContext, List<Landscape> mLandscapes) {
+        this.mContext = mContext;
+        this.mLandscapes = mLandscapes;
+        mLayoutInflater = LayoutInflater.from(this.mContext);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.list_item,parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Landscape current = mLandscapes.get(position);
+        holder.setData(current, position);
+        holder.setListeners();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mLandscapes.size();
+    }
+
+    public void removeItem(int position) {
+        //mLandscapes.removeAt(position);
+        mLandscapes.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mLandscapes.size());
+    }
+
+    public void addItem(int position, Landscape landscape) {
+        mLandscapes.add(position, landscape);
+        notifyItemInserted(position);
+        notifyItemRangeChanged(position, mLandscapes.size());
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public final TextView title;
+        public final ImageView imgThumb;
+        public final ImageView imgDelete;
+        public final ImageView imgAdd;
+        public  int pos;
+        public Landscape current;
+
+
+        public void setData(Landscape current, int position) {
+            //this.title.text = current.title;
+            this.title.setText(current.title);
+            //this.title = current.title;
+            imgThumb.setImageResource(current.imageID);
+            this.pos = position;
+            this.current = current;
+        }
+
+        public void setListeners() {/*
+            imgDelete.setOnClickListener((View.OnClickListener) this);
+            imgAdd.setOnClickListener((View.OnClickListener) this);
+            imgThumb.setOnClickListener((View.OnClickListener) this);*/
+
+        }
+
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = (TextView)itemView.findViewById(R.id.tvTitle);
+            imgThumb = (ImageView)itemView.findViewById(R.id.img_row);
+            imgDelete = (ImageView)itemView.findViewById(R.id.img_row_delete);
+            imgAdd = (ImageView)itemView.findViewById(R.id.img_row_add);
+            imgDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    removeItem(pos);
+
+                }
+            });
+            imgAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    addItem(pos, current);
+                }
+            });
+        }
+    }
+}
